d3e9943716c87978378a1808bb75d0a2d4b8962e
Flein890 <emifran2812@outlook.com>
2024-08-01 15:19:08 -0300

    email validation and roles

---
+++ helpers/DBvalidations.ts
@@ -0,0 +1,15 @@
+import User, {IUser} from "../models/user";
+
+//Creamos una funcion que devuelve una promesa a resolver que no devuelve nada
+//buscamos si existe el mail con el metodo findOne() y desestructuramos el mail
+//la variable creada tiene que ser de tipo IUser o null para que podamos buscar en User
+//en caso de q exista el mail tirar un error
+
+export const existsEmail = async(email:string):Promise<void> =>{
+
+    const existEmail: IUser | null = await User.findOne({email});
+
+    if(existEmail){
+        throw new Error('This email is registered');
+    }
+}
